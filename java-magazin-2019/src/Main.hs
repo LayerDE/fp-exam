efe429f162bac52d078c7a95b8be010064fde18b
LayerDE <the_gamer@gmx.net>
2021-09-07 18:35:33 +0200

    alias

--- java-magazin-2019/src/Main.hs
+++ java-magazin-2019/src/Main.hs
@@ -1,9 +1,9 @@
 module Main where
 
 import qualified M4337015
-import qualified Gameplay
+import qualified Gameplay as G
 
 main :: IO ()
-player4337015 = Gameplay.makePlayer "Big V (4337015)" M4337015.shootTheMoonStrategy
-main = Gameplay.startCustom1 player4337015
+player4337015 = G.makePlayer "Big V (4337015)" M4337015.shootTheMoonStrategy
+main = G.startCustom1 player4337015
 
