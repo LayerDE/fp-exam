5f536fb4678f66a1503c2d6c8b6b136afc479470
LayerDE <the_gamer@gmx.net>
2021-09-07 18:00:08 +0200

    ex9 TODO

--- java-magazin-2019/src/M0000000.hs
+++ java-magazin-2019/src/M0000000.hs
@@ -33,4 +33,24 @@ shootTheMoonStrategy =
         Nothing ->
           return (Set.findMin hand)
         Just card ->
-          return card 
+          return card
+
+
+strategy :: PlayerStrategy
+strategy =
+  PlayerStrategy $ do
+  playerState <- State.get
+  let trick = playerTrick playerState
+      hand = playerHand playerState
+      firstCard = leadingCardOfTrick trick
+      firstSuit = suit firstCard
+      followingCardsOnHand = Set.filter ((== firstSuit) . suit) hand
+  if trickEmpty trick 
+    then
+      return (Set.findMin hand)
+    else
+      case Set.lookupMin followingCardsOnHand of
+        Nothing ->
+          return (Set.findMax hand) -- any card is fine, so try to get rid of high hearts
+        Just card ->
+          return card
