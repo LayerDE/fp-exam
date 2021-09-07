4b97ec4faf25084b49d69b30d3c6f5e6978c2fee
LayerDE <the_gamer@gmx.net>
2021-09-07 18:04:38 +0200

    rename

--- java-magazin-2019/src/Main.hs
+++ java-magazin-2019/src/Main.hs
@@ -1,9 +1,9 @@
 module Main where
 
-import qualified M0000000
+import qualified M4337015
 import qualified Gameplay
 
 main :: IO ()
-player0000000 = Gameplay.makePlayer "0000000" M0000000.shootTheMoonStrategy
-main = Gameplay.startCustom1 player0000000
+player4337015 = Gameplay.makePlayer "Big V 4337015" M4337015.shootTheMoonStrategy
+main = Gameplay.startCustom1 player4337015
 
