087b331d0953ee127b455c8cebaba168c9d524cb
LayerDE <the_gamer@gmx.net>
2021-09-07 18:06:45 +0200

    rename player

--- java-magazin-2019/src/Main.hs
+++ java-magazin-2019/src/Main.hs
@@ -4,6 +4,6 @@ import qualified M4337015
 import qualified Gameplay
 
 main :: IO ()
-player4337015 = Gameplay.makePlayer "Big V 4337015" M4337015.shootTheMoonStrategy
+player4337015 = Gameplay.makePlayer "Big V (4337015)" M4337015.shootTheMoonStrategy
 main = Gameplay.startCustom1 player4337015
 
